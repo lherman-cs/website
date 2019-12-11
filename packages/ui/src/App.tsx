2d5e51c70cec7c61e4bbc303419457e8ceab3e55
Lukas Herman <lherman.cs@gmail.com>
2019-12-10 19:56:28 -0800

    Add a loading page

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,39 +1,60 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect
 } from "react-router-dom";
+import { Slide } from "@material-ui/core";
 import Landing from "./pages/landing";
 import Login from "./pages/login";
+import Loading from "./pages/loading";
 import { useAuth } from "./App.hook";
+import { makeStyles } from "@material-ui/styles";
 
-const Loading: React.FC = () => {
-  return <h1>Loading...</h1>;
-};
+const useStyles = makeStyles(() => ({
+  container: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    zIndex: 1,
+    height: "100%",
+    width: "100%",
+    overflow: "hidden"
+  }
+}));
 
 const App: React.FC = () => {
   const { initialized } = useAuth();
-
-  if (!initialized) {
-    return <Loading></Loading>;
-  }
+  const classes = useStyles();
 
   return (
-    <Router>
-      <Switch>
-        <Route path="/landing">
-          <Landing></Landing>
-        </Route>
-        <Route path="/login">
-          <Login></Login>
-        </Route>
-        <Route path="/">
-          <Redirect to="/landing"></Redirect>
-        </Route>
-      </Switch>
-    </Router>
+    <Fragment>
+      <Loading></Loading>
+      <div className={classes.container}>
+        <Slide
+          direction="up"
+          in={initialized}
+          timeout={{ enter: 1000 }}
+          mountOnEnter
+          unmountOnExit
+        >
+          <Router>
+            <Switch>
+              <Route path="/landing">
+                <Landing></Landing>
+              </Route>
+              <Route path="/login">
+                <Login></Login>
+              </Route>
+              <Route path="/">
+                <Redirect to="/landing"></Redirect>
+              </Route>
+            </Switch>
+          </Router>
+        </Slide>
+      </div>
+    </Fragment>
   );
 };
 
